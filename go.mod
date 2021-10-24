b2b0647bbffe0679fa9ca084d0be706a641596be
AnnaBushe <annbushe@yandex.ru>
2021-10-24 16:56:39 +0300

    Add bridge creation

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module VPN2.0
 
 go 1.16
+
+require (
+	github.com/google/uuid v1.3.0
+	github.com/vishvananda/netlink v1.1.0
+)
--- server/main.go
+++ server/main.go
@@ -1,18 +1,42 @@
 package server
 
 import (
-	commands "VPN2.0/cmd"
 	"bufio"
 	"fmt"
 	"io"
 	"net"
+	"os/exec"
 	"strings"
+
+	"github.com/google/uuid"
+
+	commands "VPN2.0/cmd"
 )
 
 const (
 	cmdLenLimit = 32
 	serverAddr  = "localhost:8080"
 )
+
+func createBridge(networkID string) error {
+	bridgeName := fmt.Sprintf("b-%s", networkID)
+
+	_, err := exec.Command("ip", "link", "add", "name", bridgeName, "type", "bridge").Output()
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+	fmt.Printf("bridge %s created!\n", bridgeName)
+
+	_, err = exec.Command("ip", "link", "set", bridgeName, "up").Output()
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	fmt.Printf("bridge %s is up!\n", bridgeName)
+
+	return nil
+}
 
 func RunServer() error {
 	listener, err := net.Listen("tcp", serverAddr)
@@ -31,7 +55,7 @@ func RunServer() error {
 		go handleClient(conn, errCh)
 
 		close(errCh)
-		if err := <- errCh; err != nil {
+		if err := <-errCh; err != nil {
 			return err
 		}
 	}
@@ -39,7 +63,15 @@ func RunServer() error {
 
 func createNetwork(conn net.Conn) error {
 	fmt.Println("got something")
-	_, err := conn.Write([]byte("Got your request"))
+
+	netID := uuid.New().String()[:5]
+	err := createBridge(netID)
+
+	resp := "network created"
+	if err != nil {
+		resp = "failed to process request"
+	}
+	_, err = conn.Write([]byte(resp))
 	return err
 }
 
