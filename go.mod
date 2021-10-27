222e8a1ea3f6ea4e49348eaef7813f9e450345a3
AnnaBushe <annbushe@yandex.ru>
2021-10-27 13:02:20 +0300

    Add logging to server and refactor

--- go.mod
+++ go.mod
@@ -4,5 +4,10 @@ go 1.16
 
 require (
 	github.com/google/uuid v1.3.0
-	github.com/vishvananda/netlink v1.1.0
+	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0
+	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d
+	go.uber.org/zap v1.19.1
+	golang.org/x/net v0.0.0-20211020060615-d418f374d309
+	google.golang.org/grpc v1.41.0
 )
--- server/main.go
+++ server/main.go
@@ -1,119 +1,28 @@
-package server
+package main
 
 import (
-	"bufio"
-	"fmt"
-	"io"
-	"net"
-	"os/exec"
-	"strings"
+	"context"
+	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap/ctxzap"
 
-	"github.com/google/uuid"
-
-	commands "VPN2.0/cmd"
+	"VPN2.0/server/internal/config"
+	"VPN2.0/server/internal/logs"
+	"VPN2.0/server/internal/src"
 )
 
-const (
-	cmdLenLimit = 32
-	serverAddr  = "localhost:8080"
-)
-
-func createBridge(networkID string) error {
-	bridgeName := fmt.Sprintf("b-%s", networkID)
-
-	_, err := exec.Command("ip", "link", "add", "name", bridgeName, "type", "bridge").Output()
+func main() {
+	conf, err := config.New()
 	if err != nil {
-		fmt.Println(err.Error())
-		return err
+		panic(err)
 	}
-	fmt.Printf("bridge %s created!\n", bridgeName)
 
-	_, err = exec.Command("ip", "link", "set", bridgeName, "up").Output()
+	logger := logs.BuildLogger(conf)
+	ctx := ctxzap.ToContext(context.Background(), logger)
+	logger.Info("server starting...")
+
+	src.CreateServer(ctx)
+
+	err = src.RunServer(ctx, conf.ServerAddr)
 	if err != nil {
-		fmt.Println(err)
-		return err
+		panic(err)
 	}
-	fmt.Printf("bridge %s is up!\n", bridgeName)
-
-	return nil
-}
-
-func RunServer() error {
-	listener, err := net.Listen("tcp", serverAddr)
-	if err != nil {
-		return err
-	}
-	for {
-		conn, err := listener.Accept()
-		fmt.Println("hello stranger")
-		if err != nil {
-			fmt.Println(":(")
-			continue
-		}
-
-		errCh := make(chan error, 1)
-		go handleClient(conn, errCh)
-
-		close(errCh)
-		if err := <-errCh; err != nil {
-			return err
-		}
-	}
-}
-
-func createNetwork(conn net.Conn) error {
-	fmt.Println("got something")
-
-	netID := uuid.New().String()[:5]
-	err := createBridge(netID)
-
-	resp := "network created"
-	if err != nil {
-		resp = "failed to process request"
-	}
-	_, err = conn.Write([]byte(resp))
-	return err
-}
-
-func processCmd(cmd string, conn net.Conn) error {
-	switch cmd {
-	case commands.CreateCmd:
-		return createNetwork(conn)
-	}
-
-	return nil
-}
-
-func handleClient(conn net.Conn, errCh chan error) {
-	defer func() {
-		err := conn.Close()
-		if err != nil {
-			errCh <- err
-		}
-	}()
-
-	clientReader := bufio.NewReader(conn)
-	for {
-		cmd, err := clientReader.ReadString('\n')
-		switch err {
-		case nil:
-			cmd := strings.TrimSpace(cmd)
-			fmt.Println(cmd)
-
-			err = processCmd(cmd, conn)
-			if err != nil {
-				errCh <- err
-			}
-		case io.EOF:
-			fmt.Println("client closed the connection by terminating the process")
-			return
-		default:
-			fmt.Printf("error: %v\n", err)
-			errCh <- err
-			return
-		}
-	}
-}
-
-func CreateServer() {
 }
