653fafde526068918426f41b3eadac1a28d0afd8
AnnaBushe <annbushe@yandex.ru>
2021-12-07 13:24:45 +0300

    Complete leave cmd

--- internal/client/manager.go
+++ internal/client/manager.go
@@ -2,6 +2,14 @@ package client
 
 import "VPN2.0/client/internal/config"
 
+const (
+	IDUndefined = -1
+)
 type Manager struct {
 	Config *config.Config
+	ID     int
+}
+
+func (c *Manager) SetClientID(id int) {
+	c.ID = id
 }
